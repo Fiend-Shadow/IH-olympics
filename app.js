2a0686487b4524123e55809ac870ad642fbafb3a
Tarek koushaji <tarek.koushaji@gmail.com>
2020-02-20 23:44:09 +0100

    server is ready ... without any route or view

--- app.js
+++ app.js
@@ -3,15 +3,22 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const hbs = require("hbs");
 const mongoose = require("mongoose");
-const connectMongo = require("connect-mongo");
+const hbs = require("hbs");
 const bodyParser = require("body-parser")
 
+
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+
+require("dotenv").config();
 
 const app = express();
+
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
