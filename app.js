1a0f9c0c79b417c88bc8ba73f557c1f2b978f656
Tarek koushaji <tarek.koushaji@gmail.com>
2020-02-21 09:22:46 +0100

    json is not working till now

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const bodyParser = require("body-parser")
 const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
-
+const indexRouter = require("./views/index.hbs")
 
 
 server.use(middlewares)
@@ -38,7 +38,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', router);
+app.use('/', indexRouter);
 
 
 // catch 404 and forward to error handler
