3f271e8c4f4b6ac46081227a68defd381242bc4a
Tarek koushaji <tarek.koushaji@gmail.com>
2020-02-21 09:49:08 +0100

    pages router and hbs are ready to use

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/side-page1", (req,res,next) => {
+  res.render("side-page1");
+})
+router.get("/side-page2", (req,res,next) => {
+  res.render("side-page2");
+})
 module.exports = router;
